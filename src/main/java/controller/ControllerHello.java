dcceabec6e224b41a151d25e614db83df748ec2e
vinagre9912 <jean1052@gmail.com>
2021-11-11 16:17:23 -0500

    controller hello

---
+++ src/main/java/controller/ControllerHello.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerHello {
+
+    @GetMapping(path="/hola")
+    public ResponseEntity<String> hola () {
+        return new ResponseEntity<>("hola", HttpStatus.OK);
+    }
+
+}
